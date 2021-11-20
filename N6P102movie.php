1d10bf74c17beec212389fd86953bab5de395e93
daw2-cano22 <91276972+daw2-cano22@users.noreply.github.com>
2021-11-20 01:51:17 +0100

    Add files via upload

---
+++ N6P102movie.php
@@ -0,0 +1,168 @@
+<html>
+	<head>
+		<?php
+			$db = mysqli_connect('localhost', 'root', 'Alumno_2021') or die ('Unable to connect. Check your connection parameters.');
+			mysqli_select_db($db, 'moviesite') or die(mysqli_error($db));
+				
+			if (isset($_GET['action']) && $_GET['action'] != '') {				
+			}
+			else
+			{
+				header("Location: N6P102movie.php?action=a", TRUE, 301);				
+			}
+			
+			if ($_GET['action'] == 'edit')				
+			{
+				echo '<title>' . ucfirst($_GET['action']) . 'Movie</title>';		
+				//retrieve the record's information 
+				
+				$query = 'SELECT movie_name, movie_type, movie_year, movie_leadactor, movie_director,movie_release, movie_rating FROM movie WHERE movie_id = ' . $_GET['id'];
+				
+				$result = mysqli_query($db, $query) or die(mysqli_error($db));
+				extract(mysqli_fetch_assoc($result));
+			}
+			else
+			{
+				//set values to blank
+				$movie_name = '';
+				$movie_type = 0;
+				$movie_year = date('Y');
+				$movie_leadactor = 0;
+				$movie_director = 0;
+			}
+											
+		?>
+	</head>
+	<body> 
+		<?php
+			if (isset($_GET['error']) && $_GET['error'] != '') 
+			{
+				echo '<div id="error">' . $_GET['error'] . '</div>';
+			}
+			
+			if (isset($_GET['action']) && $_GET['action'] != '') 
+			{
+				echo '<form action="N6P103commit.php?action=' . $_GET['action'] . '>&type=movie" method="post">';			
+			}
+		?>  
+			<table>
+				<tr>
+					<td>Movie Name</td>
+					<td>
+						<?php 
+							echo '<input type="text" name="movie_name" value="' . $movie_name . '"/>';
+						?> 			
+					</td>
+				</tr>
+				<tr>
+					<td>Movie Type</td>
+					<td>
+						<select name="movie_type">
+							<?php
+								// select the movie type information
+								$query = 'SELECT movietype_id, movietype_label FROM movietype ORDER BY movietype_label';
+								$result = mysqli_query($db, $query) or die(mysqli_error($db));
+
+								// populate the select options with the results
+								while ($row = mysqli_fetch_assoc($result)) 
+								{
+									if ($row['movietype_id'] == $movie_type) 
+									{
+										echo '<option value="' . $row['movietype_id'] . '" selected="selected">';
+									}
+									else 
+									{
+										echo '<option value="' . $row['movietype_id'] . '">';
+									}
+									echo $row['movietype_label'] . '</option>';
+								}
+							?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>Movie Year</td>
+					<td>
+						<select name="movie_year">
+							<?php
+								// populate the select options with years
+								for ($yr = date("Y"); $yr >= 1970; $yr--) 
+								{
+									if ($yr == $movie_year) 
+									{
+										echo '<option value="' . $yr . '" selected="selected">' . $yr . '</option>';
+									}
+									else 
+									{
+										echo '<option value="' . $yr . '">' . $yr . '</option>';
+									}
+								}
+							?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>Lead Actor</td>
+					<td>
+						<select name="movie_leadactor">
+							<?php
+							// select actor records
+							$query = 'SELECT people_id, people_fullname FROM people WHERE people_isactor = 1 ORDER BY people_fullname';
+							$result = mysqli_query($db, $query) or die(mysqli_error($db));
+
+							// populate the select options with the results
+							while ($row = mysqli_fetch_assoc($result)) {
+								foreach ($row as $value) {
+									if ($row['people_id'] == $movie_leadactor) {
+										echo '<option value="' . $row['people_id'] . '" selected="selected">';
+									} else {
+										echo '<option value="' . $row['people_id'] . '">';
+									}
+									echo $row['people_fullname'] . '</option>';
+								}
+							}
+							?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>Director</td>
+					<td>
+						<select name="movie_director">
+							<?php
+							// select director records
+							$query = 'SELECT people_id, people_fullname FROM people WHERE people_isdirector = 1 ORDER BY people_fullname';
+							$result = mysqli_query($db, $query) or die(mysqli_error($db));
+
+							// populate the select options with the results
+							while ($row = mysqli_fetch_assoc($result)) {
+								if ($row['people_id'] == $movie_director) {
+									echo '<option value="' . $row['people_id'] .
+										'" selected="selected">';
+								} else {
+									echo '<option value="' . $row['people_id'] . '">';
+								}
+								echo $row['people_fullname'] . '</option>';
+							}
+							?>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2" style="text-align: center;">
+						<?php
+							if (isset($_GET['action']) && $_GET['action'] != '') 
+							{
+								if ($_GET['action'] == 'edit') 
+								{
+									echo '<input type="hidden" value="' . $_GET['id'] . '" name="movie_id" />';
+								}
+								echo '<input type="submit" name="submit" value="' . ucfirst($_GET['action']) . '" />';
+							}
+						?>							
+					</td>
+				</tr>
+			</table>
+		</form>
+	</body>
+</html>
---
+++ N6P106comments.php
@@ -0,0 +1,25 @@
+<html>
+ <head>
+  <title>Comments</title>
+ </head>
+ <body>
+<?php 
+echo '<ul>';
+echo '<li>Regex</li>';
+echo '<li>Isset</li>';
+echo '<li>Create page stages</li>';
+echo '<li>list objects</li>';
+echo '<li>options</li>';
+echo '<li>switch cases</li>';
+echo '<li>arrays</li>';
+echo '<li>urlencode</li>';
+echo '<li>join</li>';
+echo '<li>trip</li>';
+echo '</ul>';
+?>
+<p>Documento 5</p>
+<p>Profesor 7</p>
+<p>Alumno 5</p>
+<p>No entiendo muy bien la practica</p>
+</body>
+</html>
